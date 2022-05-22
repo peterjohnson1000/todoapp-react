5188f9708b98495e204c9edd8323cfc4aeea720b
Peter Johnson <peterj1298@gmail.com>
2022-05-22 16:06:36 +0530

    defined context, action & reducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, {useState} from 'react';
-import './App.css';
-import Input from './components/Input';
-import Todolist from './components/Todolist';
+import { useReducer } from "react";
+import { Container } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { TodoContext } from './Context/TodoContext';
+import todoReducer from './Context/reducer';
 
-function App() {
-  const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState([]);
+const App = () => {
+
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
   return (
-    <div className = "container">
-      <header>
-        <h1>Peter's Todo List</h1>
-      </header>
-      <Input inputText = {inputText} setInputText = {setInputText} todos = {todos} setTodos = {setTodos} />
-      <Todolist setTodos = {setTodos} todos = {todos}/>
-    </div>
-    
+    <TodoContext.Provider value = {{todos,dispatch}}>
+      <Container fluid>
+        <h1>Peter Johndon</h1>
+      </Container>
+    </TodoContext.Provider>
   );
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState} from 'react';
+// import './App.css';
+// import Input from './components/Input';
+// import Todolist from './components/Todolist';
+
+// function App() {
+//   const [inputText, setInputText] = useState("");
+//   const [todos, setTodos] = useState([]);
+//   return (
+//     <div className = "container">
+//       <header>
+//         <h1>Peter's Todo List</h1>
+//       </header>
+//       <Input inputText = {inputText} setInputText = {setInputText} todos = {todos} setTodos = {setTodos} />
+//       <Todolist setTodos = {setTodos} todos = {todos}/>
+//     </div>
+    
+//   );
+// }
+
+// export default App;
